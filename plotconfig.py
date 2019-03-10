b1868d75a69f46e38231a19219c1339b3ba250a5
Advay Mengle <source@madvay.com>
2019-03-09 20:11:43 -0800

    Taylor series start

---
+++ plotconfig.py
@@ -0,0 +1,13 @@
+# Plot Config
+plt.rcParams['savefig.dpi'] = 192
+plt.rcParams['text.usetex'] = False
+plt.rcParams['font.family'] = "serif"
+plt.rcParams['font.serif'] = ["cm", "CMU Serif"]
+plt.rcParams['figure.autolayout'] = False
+plt.rcParams['figure.figsize'] = 6, 6
+plt.rcParams['axes.labelsize'] = 18
+plt.rcParams['axes.titlesize'] = 20
+plt.rcParams['font.size'] = 16
+plt.rcParams['lines.linewidth'] = 2.0
+plt.rcParams['lines.markersize'] = 8
+plt.rcParams['legend.fontsize'] = 14
